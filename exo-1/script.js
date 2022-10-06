65c2851becfc72ab16aef3763f6e94983ed5b59a
BeiyaC <anis.grigah@le-campus-numerique.fr>
2022-10-06 14:22:03 +0200

    Exo 1

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,8 @@
 // you can write js here
 console.log('hello from file');
+let kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+
+let celsius = kelvins - 273; // We subtract to obtain celsius value
+let fahrenheits = Math.floor(celsius * (9/5) + 32); // We use floor method to obtain an int for fahrenheits
+
+console.log(celsius, fahrenheits);
