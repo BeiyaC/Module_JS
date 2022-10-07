15242c31984144050e10c8d0f37bc0576c252e9b
BeiyaC <anis.grigah@le-campus-numerique.fr>
2022-10-07 14:27:21 +0200

    Exo 5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,37 @@
 // you can write js here
 
 console.log('exo-5');
+
+let input = prompt('Write what you want');
+
+const vowels = ['a','e','i','o','u','y'];
+
+let resultArray = [];
+
+input = input.toLowerCase();
+input = Array.from(input);
+
+function whaleSpeak(input, vowels){
+
+    for(let i = 0; i < input.length; i++){
+
+        /*for(let j = 0; j < vowels.length; j++)
+        {
+            if(input[i] === vowels[j])
+            {
+                resultArray.push(input[i]);
+                break;
+            }
+        }*/
+
+        if(vowels.indexOf(input[i]) >= 0){
+            resultArray.push(input[i]);
+        }
+
+    }
+
+    return resultArray;
+}
+
+
+console.log(whaleSpeak(input, vowels).join('').toUpperCase());
